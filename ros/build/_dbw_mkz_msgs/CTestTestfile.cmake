ed26f4586120588b325916f2086420ef583be164
Hussain Abosobaie <hussain.sobaie@gmail.com>
2019-11-10 03:14:38 +0100

    cleaning up un-needed files

--- ros/build/_dbw_mkz_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hussain/CarND-Capstone/ros/src/_dbw_mkz_msgs
-# Build directory: /home/hussain/CarND-Capstone/ros/build/_dbw_mkz_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
